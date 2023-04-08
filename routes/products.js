dfc3aec71a50970bf3ebc41af9d9e271f234a1a6
Papatoncio <juanpij23@hotmail.com>
2023-04-08 14:41:03 -0600

    Commit

--- routes/products.js
+++ routes/products.js
@@ -1,36 +1,60 @@
 var express = require("express");
 var router = express.Router();
-let product = require('../models/producto');
+let productModel = require('../models/producto');
 
 
-router.get("/", async (req, res, next) => {
-    const findAllProducts = await product.find();
-    res.json(findAllProducts);
+router.post("/", async function (req, res, next) {
+
+
+    const product = new productModel({
+        id: req.body.id, //Extra el Id pasado por el body
+        description: req.body.description,
+        name: req.body.name,
+        price: req.body.price,
+        images: req.body.images
+    });
+
+
+    const result = await product.save(); // Lo guarda en Mongo
+    res.json('Registro Agregado exitosamente');
 });
 
+//Listado de todos los productos
+router.get("/", async function (req, res, next) {
 
-router.get("/:id", async (req, res, next) => {
-    const findOneProducto = await product.findById(req.params.id);
-    res.json(findOneProducto);
+
+    const resultado = await productModel.find();
+    res.json(resultado);
 });
 
+router.delete("/:id", async function (req, res, next) {
 
-router.post("/", async (req, res, next) => {
-    const createProduct = await product.create(req.body);
-    res.json(createProduct);
+
+    //Buscar un producto por ID y regresa una lista
+    const resul = await productModel.find({ id: req.params.id }).exec();
+
+
+    //Si se encontró lo elimina
+    if (resul.length > 0) {
+        await productModel.deleteOne({ id: req.params.id });
+        res.json("Eliminando producto");
+    } else {
+        res.json({ error: "No se encontró el producto con Id " + req.params.id })
+    }
 });
 
+router.put("/", async function (req, res, next) {
+    const filter = { id: req.query.id }; //Condición de Query
+    const update = { name: req.query.name }; //Campos a modificar
 
-router.put("/:id", async (req, res, next) => {
-    const updateProduct = await product.findByIdAndUpdate(req.params.id, req.body)
-    res.json(updateProduct);
+
+    const resultado = await productModel.findOneAndUpdate(filter, update, {
+        new: true,
+        upsert: true
+    });
+
+
+    res.json("Se actualiza el producto");
 });
-
-
-router.delete("/:id", async (req, res, next) => {
-    const deleteProduct = await product.findByIdAndDelete(req.params.id);
-    res.json(deleteProduct);
-});
-
 
 module.exports = router;
